8d3546993cba0bc3aeec5bf9aff16cb02b717be0
philippedward <amelie.rakowiecki@studentfr.ch>
2025-11-16 21:43:13 +0100

    version 2 pour le boutton fail

--- src/js/main.js
+++ src/js/main.js
@@ -156,3 +156,19 @@ parallax.to(
   },
   0
 );
+
+const mainBtn = document.getElementById("mainBtn");
+const dropdown = document.getElementById("dropdown");
+
+mainBtn.addEventListener("click", function () {
+  dropdown.classList.toggle("show"); // ← toggle = ajoute OU enlève
+  mainBtn.classList.toggle("active");
+});
+
+// Fermer le dropdown si on clique ailleurs
+document.addEventListener("click", function (event) {
+  if (!event.target.closest(".book-btn-buy")) {
+    dropdown.classList.remove("show");
+    mainBtn.classList.remove("active");
+  }
+});
